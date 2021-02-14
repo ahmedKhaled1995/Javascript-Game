1d785d68c20090ae2d9e0437a07ecaea13a921c4
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-02-14 02:27:54 +0200

    direct to page 2 v3

--- js/game/gameObject.js
+++ js/game/gameObject.js
@@ -48,8 +48,8 @@ class GameObject{
         if(this.img === undefined){
             this.drawObject();
         }else{
-            console.log(this.img);
-            console.log("---");
+            //console.log(this.img);
+            //console.log("---");
             this.context.drawImage(this.img, this.startX, this.startY, this.width, this.height);
             //this.drawCollider();   // debugging only
         }
