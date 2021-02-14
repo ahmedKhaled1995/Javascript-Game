74d0bd3e1a8ce0f7cc5624f5f3d8dfca7c9bcd4e
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-02-15 00:04:02 +0200

    Final deployment2 to 1 _2

--- js/cursorControl.js
+++ js/cursorControl.js
@@ -1,35 +1,28 @@
 const CURSOR = document.querySelector(".cursor");
 const BTNS = document.querySelectorAll(".btn");
-const TITLE = document.querySelector('.title');
+// var timeOut;
 
 document.addEventListener("mousemove", (e) => {
     let x = e.pageX;
     let y = e.pageY;
 
-    CURSOR.style.top = y + 30 + "px";     //adding an offset to accuratley position the cursor over the rocket
+    CURSOR.style.top = y + "px";
     CURSOR.style.left = x + "px";
     CURSOR.style.display = "block";
+
+    //cursor effects when mouse stopped
+    function mouseStopped() {
+        CURSOR.style.display = "none";
+    }
+    // clearTimeout(timeout);
+    // timeout = setTimeout(mouseStopped, 1000);
+});
+
+BTNS.forEach((eachBtn) => {
+    eachBtn.addEventListener('mouseenter',  () => eachBtn.style.cursor = "none");
+    eachBtn.addEventListener('mousemove', () => eachBtn.style.cursor = "none");
 });
 
 document.addEventListener("mouseout", () => {
     CURSOR.style.display = "none";
 });
-
-
-BTNS.forEach((eachBtn) => {
-    eachBtn.addEventListener('mouseenter', () => {
-        eachBtn.style.cursor = "none";
-        eachBtn.style.color = "white";
-        eachBtn.style.fontWeight = "bold";
-        CURSOR.classList.add("over-btn");
-    });
-    // eachBtn.addEventListener('mousemove', () => eachBtn.style.cursor = "none");
-    eachBtn.addEventListener('mouseleave', () => {
-        eachBtn.style.color = "aquamarine";
-        eachBtn.style.fontWeight = "normal";
-        CURSOR.classList.remove("over-btn");
-    });
-});
-
-TITLE.addEventListener('mouseover', () => TITLE.classList.add("title-grow"));
-TITLE.addEventListener('mouseleave', () => TITLE.classList.remove("title-grow"));
